c65eec403d41336730093789da70e6bda7df4fcb
Paul Berberian <paul.berberian@proton.me>
2026-01-18 12:54:39 +0100

    ppu: doc update after clippy

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -8,15 +8,15 @@ use frame::FrameRenderer;
 
 use registers::NesPpuRegister;
 
-/// $0000-$0FFF 	$1000 	Pattern table 0
-/// $1000-$1FFF 	$1000 	Pattern table 1
-/// $2000-$23FF 	$0400 	Nametable 0
-/// $2400-$27FF 	$0400 	Nametable 1
-/// $2800-$2BFF 	$0400 	Nametable 2
-/// $2C00-$2FFF 	$0400 	Nametable 3
-/// $3000-$3EFF 	$0F00 	Mirrors of $2000-$2EFF
-/// $3F00-$3F1F 	$0020 	Palette RAM indexes
-/// $3F20-$3FFF 	$00E0 	Mirrors of $3F00-$3F1F
+/// $0000-$0FFF     $1000     Pattern table 0
+/// $1000-$1FFF     $1000     Pattern table 1
+/// $2000-$23FF     $0400     Nametable 0
+/// $2400-$27FF     $0400     Nametable 1
+/// $2800-$2BFF     $0400     Nametable 2
+/// $2C00-$2FFF     $0400     Nametable 3
+/// $3000-$3EFF     $0F00     Mirrors of $2000-$2EFF
+/// $3F00-$3F1F     $0020     Palette RAM indexes
+/// $3F20-$3FFF     $00E0     Mirrors of $3F00-$3F1F
 const CHR_ROM_START: u16 = 0x0000;
 const CHR_ROM_END: u16 = 0x1FFF;
 const PALETTE_RAM_START: u16 = 0x03F00;
